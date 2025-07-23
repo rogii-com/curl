e273238ed715e013a2a37a6e62013e7d80b66be1
Alexander Shkarlatov <crazyalex25@mail.ru>
2025-07-23 15:26:25 +0400

    Use library as static

--- rogii/package.cmake
+++ rogii/package.cmake
@@ -2,52 +2,29 @@ if(TARGET libcurl)
     return()
 endif()
 
-add_library(libcurl SHARED IMPORTED)
-add_library(CURL::libcurl_shared ALIAS libcurl)
+add_library(libcurl STATIC IMPORTED)
+add_library(CURL::libcurl_static ALIAS libcurl)
 
 if(MSVC)
     set_target_properties(
         libcurl
         PROPERTIES
             IMPORTED_LOCATION
-                "${CMAKE_CURRENT_LIST_DIR}/bin/libcurl.dll"
+                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurl.lib"
             IMPORTED_LOCATION_DEBUG
-                "${CMAKE_CURRENT_LIST_DIR}/bin/libcurld.dll"
+                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurld.lib"
             INTERFACE_INCLUDE_DIRECTORIES
                 "${CMAKE_CURRENT_LIST_DIR}/include"
-            IMPORTED_IMPLIB
-                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurl_imp.lib"
-            IMPORTED_IMPLIB_DEBUG
-                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurld_imp.lib"
     )
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     set_target_properties(
         libcurl
         PROPERTIES
             IMPORTED_LOCATION
-                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurl.so"
+                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurl.a"
             IMPORTED_LOCATION_DEBUG
-                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurld.so"
+                "${CMAKE_CURRENT_LIST_DIR}/lib/libcurld.a"
             INTERFACE_INCLUDE_DIRECTORIES
                 "${CMAKE_CURRENT_LIST_DIR}/include"
     )
 endif()
-
-set(
-    COMPONENT_NAMES
-
-    CNPM_RUNTIME_libcurl
-    CNPM_RUNTIME
-)
-
-foreach(COMPONENT_NAME ${COMPONENT_NAMES})
-    install(
-        FILES
-            $<TARGET_FILE:libcurl>
-        DESTINATION
-            .
-        COMPONENT
-            ${COMPONENT_NAME}
-        EXCLUDE_FROM_ALL
-    )
-endforeach()
