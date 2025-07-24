cf9c418bd737d56b4a6856a2d61c0d16de0a9365
Alexander Shkarlatov <crazyalex25@mail.ru>
2025-07-24 16:52:06 +0400

    Zlib support

--- rogii/environment_windows.cmake
+++ rogii/environment_windows.cmake
@@ -1,2 +1,13 @@
 include(${CMAKE_CURRENT_LIST_DIR}/msvs_package.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/windowssdk_package.cmake)
+
+CNPM_ADD_PACKAGE(
+    NAME
+        ZLib
+    VERSION
+        1.2.8
+    BUILD_NUMBER
+        8
+    TAG
+        "sdk22621_vsbt22"
+)
